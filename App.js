9c97178567fa3a750003a78cea08fc76ee39d53d
flaviaglenda <flaviaglenda15@gmail.com>
2025-08-28 14:44:26 -0300

    bottom tabs

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons'; // ícones prontos
+
+import homeScreen from './src/screens/homeScreen';
+import carrinhoCompras from './src/screens/carrinhoCompras';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'homeScreen') {
+              iconName = 'home-outline';
+            } else if (route.name === 'carrinhoCompras') {
+              iconName = 'cart-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#2e8b57', // cor quando selecionado
+          tabBarInactiveTintColor: 'gray',  // cor quando não tá selecionado
+        })}
+      >
+        <Tab.Screen name="homeScreen" component={homeScreen} />
+        <Tab.Screen name="carrinhoCompras" component={carrinhoCompras} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
